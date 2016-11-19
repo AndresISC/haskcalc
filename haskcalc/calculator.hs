d64d77a2def38d2492e15b0e35353b2c4d006f73
AndresISC <ach1593@gmail.com>
2016-11-18 23:57:29 -0600

    Primer commit

---
+++ haskcalc/calculator.hs
@@ -0,0 +1,38 @@
+module Main where
+sq :: Float->Float
+sq c = c * c
+
+normalize :: Float->Float
+normalize a = a - (fromIntegral (truncate (a / (3.1415926535897932384626433832795/2)))*(3.1415926535897932384626433832795/2))
+
+tobin :: Integer->String
+tobin decimal = (if decimal < 2 then (show decimal) else (tobin (toInteger (truncate ((fromIntegral(decimal))/2)))) ++ show (mod decimal 2))
+
+todec :: 
+
+--TODO: To decimal from binary (I'll do this)
+--Trigonometric functions ( :v I'll do diz 2)
+
+suma :: Float->Float->Float
+suma a b = a + b
+
+rest :: Float->Float->Float
+rest a b = a - b
+
+mult :: Float->Float->Float
+mult a b = a * b
+
+divs :: Float->Float->Float
+divs a b = a / b 
+
+main =  do
+         a<- readLn :: IO Float
+         b<- readLn :: IO Float
+         let s = suma a b
+         let r = rest a b
+         let m = mult a b
+         let d = divs a b
+         print s
+         print r
+         print m
+         print d
