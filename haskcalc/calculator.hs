9fde9b89d9fc31e538cb9db9bb076c87a4d4e053
AndresISC <ach1593@gmail.com>
2016-11-19 15:57:48 -0600

    Hola :V

--- haskcalc/calculator.hs
+++ haskcalc/calculator.hs
@@ -3,12 +3,35 @@ sq :: Float->Float
 sq c = c * c
 
 normalize :: Float->Float
-normalize a = a - (fromIntegral (truncate (a / (3.1415926535897932384626433832795/2)))*(3.1415926535897932384626433832795/2))
+normalize a = 
+   a - (fromIntegral (truncate (a / (3.1415926535897932384626433832795/2)))*(3.1415926535897932384626433832795/2))
 
 tobin :: Integer->String
-tobin decimal = (if decimal < 2 then (show decimal) else (tobin (toInteger (truncate ((fromIntegral(decimal))/2)))) ++ show (mod decimal 2))
+tobin decimal = 
+   if decimal > 0 then
+    show 0 ++ (getAbsBin decimal) 
+   else
+    if decimal < 0 then
+      show 1 ++ (getAbsBin decimal)
+    else 
+      (show 0) 
 
-todec :: 
+complete16bit :: String->String
+complete16bit binarydig = 
+   if (length (binarydig)) < 16 then 
+      (complete16bit (show 0 ++ binarydig)) 
+   else 
+      binarydig
+
+getAbsBin :: Integer->String
+getAbsBin decimal = 
+   if decimal < 2 then 
+      (show decimal) 
+   else 
+      (getAbsBin (toInteger (truncate ((fromIntegral(decimal))/2)))) ++ show (mod decimal 2)
+
+compbin :: String->String
+compbin cadena = if (length cadena) < 16 then (compbin (show 0 ++ cadena)) else cadena
 
 --TODO: To decimal from binary (I'll do this)
 --Trigonometric functions ( :v I'll do diz 2)
