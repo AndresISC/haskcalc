1ec22de1de8d780ec74a89f33689981f5bd3057f
José Carlos López <jcl.isc@gmail.com>
2016-11-20 23:12:02 -0600

    binaryAdd

--- haskcalc/calculator.hs
+++ haskcalc/calculator.hs
@@ -1,36 +1,39 @@
 module Main where
+
+import BinUtils
+
 sq :: Float->Float
 sq c = c * c
 
 normalize :: Float->Float
-normalize a = 
+normalize a =
    a - (fromIntegral (truncate (a / (3.1415926535897932384626433832795/2)))*(3.1415926535897932384626433832795/2))
 
 tobin :: Integer->String
-tobin decimal = 
+tobin decimal =
    if (abs (decimal)) <= 32767 then
       if decimal > 0 then
-    show 0 ++ (complete16bit (getAbsBin decimal)) 
+    show 0 ++ (complete16bit (getAbsBin decimal))
    else
     if decimal < 0 then
       show 1 ++ (complete16bit (getAbsBin (abs (decimal))))
-    else 
+    else
       (show 0)
    else
       "Error, only numbers between (-32768,32768)"
 
 complete16bit :: String->String
-complete16bit binarydig = 
-   if (length (binarydig)) < 15 then 
-      (complete16bit (show 0 ++ binarydig)) 
-   else 
+complete16bit binarydig =
+   if (length (binarydig)) < 15 then
+      (complete16bit (show 0 ++ binarydig))
+   else
       binarydig
 
 getAbsBin :: Integer->String
-getAbsBin decimal = 
-   if decimal < 2 then 
-      (show decimal) 
-   else 
+getAbsBin decimal =
+   if decimal < 2 then
+      (show decimal)
+   else
       (getAbsBin (toInteger (truncate ((fromIntegral(decimal))/2)))) ++ show (mod decimal 2)
 
 compbin :: String->String
@@ -49,7 +52,7 @@ mult :: Float->Float->Float
 mult a b = a * b
 
 divs :: Float->Float->Float
-divs a b = a / b 
+divs a b = a / b
 
 main =  do
          a<- readLn :: IO Float
