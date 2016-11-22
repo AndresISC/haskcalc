fb6be34119a9cd3b6e8c256a0e9febae1ddf856e
José Carlos López <jcl.isc@gmail.com>
2016-11-21 18:50:03 -0600

    binToInt unsigned

--- haskcalc/Utils/Strings.hs
+++ haskcalc/Utils/Strings.hs
@@ -1,4 +1,11 @@
-module Utils.Strings where
+module Util.Strings
+    (
+    charToBool,
+    boolToChar,
+    stringToBools,
+    boolsToString,
+    binToInt
+    ) where
 
   {-|
   Function to convert any character into a boolean.
@@ -34,3 +41,22 @@ module Utils.Strings where
   -}
   boolsToString :: [Bool] -> String
   boolsToString bools = [ c | bool <- bools, let c = boolToChar bool ]
+
+
+  {-|
+  Converts a binary list to an Integer (unsigned)
+  -}
+  binToInt :: [Bool] -> Int
+  binToInt bools =
+    let
+      reversed = reverse bools
+      maxPow = length bools - 1
+    in _binToInt reversed maxPow
+
+  {-|
+  Converts a binary number to an Integer (unsigned)
+  -}
+  _binToInt :: [Bool] -> Int -> Int
+  _binToInt bools place
+    | place >= 0 = if bools !! place then (2^place) + _binToInt bools (place - 1) else 0 + _binToInt bools (place - 1)
+    | otherwise = 0
