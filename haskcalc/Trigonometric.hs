46472f4b4ea7ab7997b1a782eae1e05aa316c2b9
AndresISC <ach1593@gmail.com>
2016-11-23 09:58:44 -0600

    Creación del módulo de funciones trogonométricas y agregación de las funciones factorial y potencia al módulo Arithmetic

---
+++ haskcalc/Trigonometric.hs
@@ -0,0 +1,76 @@
+module Trigonometric(
+  msen,
+  mcos,
+  mtan,
+  mcot,
+  msec,
+  mcsc
+  )
+where
+
+{-|
+  --Get the value equivalent to radians of the presented degrees
+-}
+torad :: Double->Double
+torad v = ( (v * pi) / 180 )
+
+{-|
+  --Recursive to get the value of sinus of the entering radians
+-}
+sinusr :: Integer->Double->Double->Double
+sinusr k x result = 
+  if k == 130 then
+   ( ((pow (-1) k ) * ((pow x ((2*k)+1))) / (fromIntegral (f ((2*k)+1)))))
+  else
+   result + (sinusr (k+1) x (((pow (-1) k )*((pow x ((2*k)+1)))/(fromIntegral (f ((2*k)+1))))))
+
+{-|
+  --Returns the "sin" of the entering value, with 'd' to indicate degrees, and you can use anything else to make it work with radians
+-}
+msen :: Double->Char->Double
+msen n deg = 
+   if(deg == 'd')then
+      sinusr 0 (normalize (torad n)) 0 
+   else
+      sinusr 0 (normalize n) 0 
+
+{-|
+  --Returns the "cos" of the entering value, with 'd' to indicate degrees, and you can use anything else to make it work with radians
+-}
+mcos :: Double->Char->Double
+mcos n deg = 
+   msen  ((pi/2)-n) deg
+
+{-|
+  --Returns the "tan" of the entering value, with 'd' to indicate degrees, and you can use anything else to make it work with radians
+-}
+mtan :: Double->Char->Double
+mtan n deg = 
+   (msen  n deg)/(mcos  n deg)
+
+{-|
+  --Returns the "cot" of the entering value, with 'd' to indicate degrees, and you can use anything else to make it work with radians
+-}
+mcot :: Double->Char->Double
+mcot n deg = 
+   1 / (mtan  n deg)
+
+{-|
+  --Returns the "sec" of the entering value, with 'd' to indicate degrees, and you can use anything else to make it work with radians
+-}
+msec :: Double->Char->Double
+msec n deg = 
+   1 / (mcos  n deg)
+
+{-|
+  --Returns the "sec" of the entering value, with 'd' to indicate degrees, and you can use anything else to make it work with radians
+-}
+mcsc :: Double->Char->Double
+mcsc n deg = 
+   1 / (msen  n deg)
+
+{-|
+  --Subtracts pi/2 the necessary times to get a value minor or equals to pi/2
+-}
+normalize :: Double->Double
+normalize a = a - (fromIntegral (truncate (a / (pi*2)))*(pi*2))
--- haskcalc/calculator.hs
+++ haskcalc/calculator.hs
@@ -18,62 +18,11 @@ f n =
    else 
       n * (f (n-1))
 
---TODO: Trigonometric functions ( :v I'll do diz 2)
-
---Get the value equivalent to radians of the presented degrees
-torad :: Double->Double
-torad v = ( (v * pi) / 180 )
-
---Recursive to get the value of sinus of the entering radians
-sinusr :: Integer->Double->Double->Double
-sinusr k x result = 
-  if k == 130 then
-   ( ((pow (-1) k ) * ((pow x ((2*k)+1))) / (fromIntegral (f ((2*k)+1)))))
-  else
-   result + (sinusr (k+1) x (((pow (-1) k )*((pow x ((2*k)+1)))/(fromIntegral (f ((2*k)+1))))))
-
---Returns the "sin" of the entering value, with 'd' to indicate degrees, and everything else to make it work with radians
-msen :: Double->Char->Double
-msen n deg = 
-   if(deg == 'd')then
-      sinusr 0 (normalize (torad n)) 0 
-   else
-      sinusr 0 (normalize n) 0 
-
---Returns the "cos" of the entering value, with 'd' to indicate degrees, and everything else to make it work with radians
-mcos :: Double->Char->Double
-mcos n deg = 
-   msen  ((pi/2)-n) deg
-
---Returns the "tan" of the entering value, with 'd' to indicate degrees, and everything else to make it work with radians
-mtan :: Double->Char->Double
-mtan n deg = 
-   (msen  n deg)/(mcos  n deg)
-
---Returns the "cot" of the entering value, with 'd' to indicate degrees, and everything else to make it work with radians
-mcot :: Double->Char->Double
-mcot n deg = 
-   1 / (mtan  n deg)
-
---Returns the "sec" of the entering value, with 'd' to indicate degrees, and everything else to make it work with radians
-msec :: Double->Char->Double
-msec n deg = 
-   1 / (mcos  n deg)
-
---Returns the "sec" of the entering value, with 'd' to indicate degrees, and everything else to make it work with radians
-mcsc :: Double->Char->Double
-mcsc n deg = 
-   1 / (msen  n deg)
-
---Subtracts pi/2 the necessary times to get a value minor or equals to pi/2 :v
-normalize :: Double->Double
-normalize a = a - (fromIntegral (truncate (a / (pi*2)))*(pi*2))
-
 --CONCERNING TO BINARY MANIPULATION
 --Converts a decimal digit between (-32768,32768) to binary
 tobin :: Integer->String
 tobin dec =
-   if (abs (dec)) <= 32767 then
+   if (abs (dec)) <= 65535 then
       if dec > 0 then
     show 0 ++ (complete16bit (getAbsBin dec))
    else
@@ -113,7 +62,7 @@ absbin decimal =
    else 
       (absbin (toInteger (truncate ((fromIntegral(decimal))/2)))) ++ show (mod decimal 2)
 complete16bit binarydig =
-   if (length (binarydig)) < 15 then
+   if (length (binarydig)) < 31 then
       (complete16bit (show 0 ++ binarydig))
    else
       binarydig
