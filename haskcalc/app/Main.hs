7be694575e36e12df4967ccfb50a934db3e8b723
José Carlos López <jcl.isc@gmail.com>
2016-11-28 23:58:13 -0600

    test command wrappers with args from commandline

--- haskcalc/app/Main.hs
+++ haskcalc/app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
+import System.Environment
+import System.IO
+import System.IO.Error
 import Arithmetic
 
 main :: IO ()
-main = print (addThem 12 12)
+main = do
+  args <- getArgs
+  putStrLn (handler args)
+
+
+handler :: [String] -> String
+handler args
+  | null args = "No parameters given."
+  | head args == "addThem" = let --addThem
+    a = read (args !! 1) :: Int
+    b = read (args !! 2) :: Int
+    in show (addThem a b)
+  | head args == "subtractThem" = let
+    a = read (args !! 1) :: Int
+    b = read (args !! 2) :: Int
+    in show (subtractThem a b)
+  | otherwise = "Unknown command"
